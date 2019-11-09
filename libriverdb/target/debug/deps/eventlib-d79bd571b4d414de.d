465a57bfe6d5141b061ea313ba463b13ae60a76d
Eloff <dan.eloff@gmail.com>
2019-11-08 17:50:30 -0800

    initial commit

---
+++ libriverdb/target/debug/deps/eventlib-d79bd571b4d414de.d
@@ -0,0 +1,5 @@
+/mnt/nvme/projects/eventstream/eventlib/target/debug/deps/eventlib-d79bd571b4d414de: src/main.rs
+
+/mnt/nvme/projects/eventstream/eventlib/target/debug/deps/eventlib-d79bd571b4d414de.d: src/main.rs
+
+src/main.rs:
---
+++ libriverdb/target/debug/eventlib.d
@@ -0,0 +1,1 @@
+/mnt/nvme/projects/eventstream/eventlib/target/debug/eventlib: /mnt/nvme/projects/eventstream/eventlib/src/main.rs
